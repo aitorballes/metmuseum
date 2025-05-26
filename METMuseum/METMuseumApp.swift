bcbb1d1d6d499c44b6415135b90f26fb2be40649
Aitor Ballesteros <aitor.ballesteros@appspace.com>
2025-05-26 17:45:16 +0200

    Initial Commit

---
+++ METMuseum/METMuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  METMuseumApp.swift
+//  METMuseum
+//
+//  Created by APPSPACE on 26/5/25.
+//
+
+import SwiftUI
+
+@main
+struct METMuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
